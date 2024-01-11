079bae7e79df5cc836ac3aa43f77ef15b17d2829
H3wastooshort <hacker3000@posteo.org>
2024-01-11 20:14:10 +0100

    add bom conversion script

---
+++ hardware/jlcpcb/bom_convert.py
@@ -0,0 +1,14 @@
+import csv
+with open("production_files/BOM-the_extender.csv",'r') as f:
+    reader=csv.reader(f)
+    with open("production_files/BOM-the_extender_lcsc.csv",'w') as fo:
+        writer=csv.writer(fo)
+        for r in reader:
+            n=r[1].count(',')+1
+            n*=1#how many should there be parts for?
+            ro=[r[0],r[3],n]
+            writer.writerow(ro)
+            #print(r[1]+" -> "+str(n))
+            print(ro)
+        
+
