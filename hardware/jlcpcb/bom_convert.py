6cc8be5a12303daacc466f35e79b88c9559b70ac
H3wastooshort <hacker3000@posteo.org>
2024-01-18 12:51:19 +0100

    [bom script] count components

--- hardware/jlcpcb/bom_convert.py
+++ hardware/jlcpcb/bom_convert.py
@@ -1,4 +1,5 @@
 import csv
+count=0
 with open("production_files/BOM-the_extender.csv",'r') as f:
     reader=csv.reader(f)
     with open("production_files/BOM-the_extender_lcsc.csv",'w') as fo:
@@ -10,5 +11,8 @@ with open("production_files/BOM-the_extender.csv",'r') as f:
             writer.writerow(ro)
             #print(r[1]+" -> "+str(n))
             print(ro)
-        
+            count += n
+
+print()
+print("Component Count: " + str(count))
 
